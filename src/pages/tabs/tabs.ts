9817cd314d9e6fb23b92f97d4a250169ab7f6cc7
Roger Florat <rflorat@cidiverte.it>
2017-05-30 10:24:31 +0200

    Tabs Page

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular';
 
-/**
- * Generated class for the TabsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-tabs',
@@ -14,11 +8,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TabsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TabsPage');
-  }
+  tab1: any = 'HomePage';
+  tab2: any = 'CategoriasPage';
+  tab3: any = 'OrdenesPage';
 
 }
